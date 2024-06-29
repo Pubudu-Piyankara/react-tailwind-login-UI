ff0898559210e7cdbb67d0ddd3226aa13d1148fd
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2024-06-29 10:28:49 +0530

    background UI

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+      
+    'node_modules/flowbite-react/lib/esm/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
